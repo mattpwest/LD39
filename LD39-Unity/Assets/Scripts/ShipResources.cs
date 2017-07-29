5d5607d636323572cf2cf6172077da254c03e2bf
Matt Van Der Westhuizen <mattpwest@gmail.com>
2017-07-29 11:41:16 +0200

    Built UI for the mine event and basic trigger of event dialogs.

--- LD39-Unity/Assets/Scripts/ShipResources.cs
+++ LD39-Unity/Assets/Scripts/ShipResources.cs
@@ -1,19 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 public class ShipResources : MonoBehaviour
 {
     public float MaxPower = 200.0f;
-    
+    public float WorkerPowerConsumption = 0.5f;
+    public float FighterPowerConsumption = 0.7f;
 
     public float CurrentPower { get; private set; }
     public float TimeLeft { get; private set; }
+    public int Population { get; private set; }
+    public int Workers { get; private set; }
+    public int Fighters { get; private set; }
+
+    public int PopulationAvailable => this.Population - this.Workers - this.Fighters;
+    public float CurrentPowerConsumption => this.CurrentWorkerPowerConsumption + this.CurrentFighterPowerConsumption;
+
+    private float CurrentWorkerPowerConsumption => this.Workers * this.WorkerPowerConsumption;
+    private float CurrentFighterPowerConsumption => this.Fighters * this.FighterPowerConsumption;
 
     public ShipResources()
     {
         this.CurrentPower = 0.0f;
-        this.TimeLeft = 1000;
+        this.TimeLeft = 1000.0f;
+        this.Population = 100;
     }
 
     public void ChargeShip(StarPower star, int timeCharging)
@@ -36,5 +46,25 @@ public class ShipResources : MonoBehaviour
     public void TimePassed(float time)
     {
         this.TimeLeft -= time;
+    }
+
+    public void SetActiveWorkers(int count)
+    {
+        if (count > this.Population - Fighters)
+        {
+            throw new ArgumentOutOfRangeException("Available population is less than the count to activate!");
+        }
+
+        this.Workers = count;
+    }
+
+    public void SetActiveFighters(int count)
+    {
+        if (count > this.Population - this.Workers)
+        {
+            throw new ArgumentOutOfRangeException("Available population is less than the count to activate!");
+        }
+
+        this.Fighters = count;
     }
 }
---
+++ LD39-Unity/Assets/Scripts/UI/HidePanelOnStart.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HidePanelOnStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	    this.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void HidePanel()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
