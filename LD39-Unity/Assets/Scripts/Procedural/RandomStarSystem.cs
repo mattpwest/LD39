d7d67b6a8e52cdf2f37660c13c008346c629b4fb
Matt Van Der Westhuizen <mattpwest@gmail.com>
2017-09-04 17:46:21 +0200

    Very basic generation of a random star system (star only so far).

---
+++ LD39-Unity/Assets/Scripts/Procedural/RandomStarSystem.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class RandomStarSystem : MonoBehaviour
+{
+
+	public int Seed;
+	public Transform StarTemplate;
+	public Transform PlanetTemplate;
+	
+	void Start () {
+		Random.InitState(Seed);
+
+		GenerateStar();
+
+		GeneratePlanets();
+	}
+
+	private void GenerateStar()
+	{
+		var star = Instantiate(StarTemplate, transform.position, Quaternion.identity, transform);
+		var starPower = star.GetComponent<StarPower>();
+		var starRenderer = star.GetComponent<SpriteRenderer>();
+
+		var scale = 1.0f + Random.Range(0.0f, 2.0f);
+		star.localScale = new Vector3(scale, scale, 1.0f);
+		starRenderer.color = Random.ColorHSV();
+		starPower.MaxDistance = 10 + Random.Range(0, 10);
+		starPower.PowerPerTime = 5 + Random.Range(0, 5);
+	}
+
+	private void GeneratePlanets()
+	{
+	}
+	
+	void Update () {
+	}
+}
